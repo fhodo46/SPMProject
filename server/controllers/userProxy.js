4efb48423b6157ee6e8754235a345926d527d700
Fabiona Tafciu <ftafciu21@epoka.edu.al>
2024-04-23 00:24:27 +0200

    other controllers

---
+++ server/controllers/userProxy.js
@@ -0,0 +1,66 @@
+const User = require("./models./user");
+
+async function logIn(username, password) {
+  const user = await getUserByUsername(username);
+  if (user) {
+    if (user.password === password) {
+      return { success: true, user: user };
+    } else {
+      return { success: false, error: "Incorrect password" };
+    }
+  } else {
+    return { success: false, error: "User not found" };
+  }
+}
+
+async function getUserByUsername(username) {
+  const user = await User.find({ username: username });
+  return user;
+}
+
+async function logOut(userId) {}
+
+async function deleteUser(userId) {
+  const user = await User.findById(userId);
+  await User.deleteOne();
+}
+
+function editUser(userId, newInfo) {
+  const response = { result: true, message: "Edited successfully!" };
+  User.findOneAndUpdate({ _id: userId }, { ...newInfo }, (err) => {
+    if (err) {
+      response["result"] = false;
+      response["message"] = "Unable to edit due to an error!";
+    }
+  });
+  return response;
+}
+
+async function changePassword(userId, newPassword, oldPassword) {
+  const user = await User.findById(userId);
+  const response = { result: true, message: "Edited successfully!" };
+  if (user) {
+    if (oldPassword === user.password) {
+      if (newPassword !== oldPassword) {
+        User.findOneAndUpdate({ _id: userId }, { newPassword }, (err) => {
+          if (err) {
+            response["result"] = false;
+            response["message"] = "Unable to update due to an error!";
+          }
+        });
+        return response;
+      } else {
+        response["result"] = false;
+        response["message"] = "New password same with old password";
+      }
+    } else {
+      response["result"] = false;
+      response["message"] = "Wrong password entered";
+    }
+  } else {
+    response["result"] = false;
+    response["message"] = "User does not exist!";
+  }
+}
+
+module.exports = { logIn, logOut, deleteUser, editUser, changePassword };
